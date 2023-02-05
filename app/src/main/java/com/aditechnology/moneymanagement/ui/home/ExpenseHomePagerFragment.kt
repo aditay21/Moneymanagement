8bf200bb313b9d7a8dbba15bdc4ac40b972ab79d
aditay <adi.21dec@gmail.com>
2023-02-05 09:18:59 +0530

    menu work

--- app/src/main/java/com/aditechnology/moneymanagement/ui/home/ExpenseHomePagerFragment.kt
+++ app/src/main/java/com/aditechnology/moneymanagement/ui/home/ExpenseHomePagerFragment.kt
@@ -1,16 +1,17 @@
 package com.aditechnology.moneymanagement.ui.home
 
-import android.R
-import android.R.attr.button
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aditechnology.moneymanagement.MainApplication
+import com.aditechnology.moneymanagement.R
 import com.aditechnology.moneymanagement.databinding.FragmentExpenseIncomeFragmentBinding
 import com.aditechnology.moneymanagement.models.DetailsFileTable
 import com.aditechnology.moneymanagement.ui.adapter.ExpenseIncomeDetailListAdapter
@@ -44,10 +45,7 @@ class ExpenseHomePagerFragment :Fragment(),ExpenseIncomeDetailListAdapter.OnClic
         }
 
         binding.textViewCurrentDateYearSelection.text = DateTimeUtils.getDate()
-        expenseIncomeViewModel.getByAccountIdAndDate(accountId,DateTimeUtils.getTimeStampFromDate(DateTimeUtils.getDate()).toString())?.observe(requireActivity()){
-                all->
-            mAccountListAdapter.updateList(all.reversed())
-        }
+        setObservers(DateTimeUtils.getTimeStampFromDate(DateTimeUtils.getDate()).toString())
 
         binding.imageViewPrevious.setOnClickListener {
             val currentDateSearch =  binding.textViewCurrentDateYearSelection.text.toString()
@@ -55,16 +53,30 @@ class ExpenseHomePagerFragment :Fragment(),ExpenseIncomeDetailListAdapter.OnClic
             val searchDateTime = DateTimeUtils.getDateFromTimeStamp(searchDate.toString())
             binding.textViewCurrentDateYearSelection.text=searchDateTime
             if (searchDate != null) {
-                expenseIncomeViewModel.getByAccountIdAndDate(accountId,searchDate)?.observe(requireActivity()){all->
-                    mAccountListAdapter.updateList(all.reversed())
-
-                }
+                setObservers(searchDate)
             }
         }
-        binding.imageViewFiltter.setOnClickListener({
+        binding.imageViewFiltter.setOnClickListener {
             val popupMenu = PopupMenu(requireContext(), binding.imageViewFiltter)
-            popupMenu.getMenuInflater().inflate(R.menu., popupMenu.getMenu())
-        })
+            popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                Toast.makeText(
+                requireContext(),
+                    "You Clicked " + menuItem.title,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                when(menuItem.itemId){
+                    menuItem.->{
+
+                    }
+                }
+                true
+
+
+
+            }
+        }
         binding.imageViewNext.setOnClickListener {
             val currentDateSearch =  binding.textViewCurrentDateYearSelection.text.toString()
             val searchDate = DateTimeUtils.getCalculatedNextDayDateTimeStamp(DateTimeUtils.
@@ -72,10 +84,7 @@ class ExpenseHomePagerFragment :Fragment(),ExpenseIncomeDetailListAdapter.OnClic
             val searchDateTime = DateTimeUtils.getDateFromTimeStamp(searchDate.toString())
             binding.textViewCurrentDateYearSelection.text=searchDateTime
             if (searchDate != null) {
-                expenseIncomeViewModel.getByAccountIdAndDate(accountId,searchDate)?.observe(requireActivity()){all->
-                    mAccountListAdapter.updateList(all.reversed())
-
-                }
+                setObservers(searchDate)
             }
 
         }
@@ -86,6 +95,17 @@ class ExpenseHomePagerFragment :Fragment(),ExpenseIncomeDetailListAdapter.OnClic
         binding.recycleView.layoutManager = linearLayoutManager1
         binding.recycleView.adapter = mAccountListAdapter
     }
+
+    private fun setObservers(searchDate: String?) {
+        if (searchDate != null) {
+            expenseIncomeViewModel.getByAccountIdAndDate(accountId, searchDate)
+                ?.observe(requireActivity()) { all ->
+                    mAccountListAdapter.updateList(all.reversed())
+
+                }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
